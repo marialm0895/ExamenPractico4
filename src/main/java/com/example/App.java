d71d7ad38cb5ef9d57917129ea4021b92e459db6
marialm0895 <marialm_95@hotmail.com>
2024-06-08 19:40:57 +0200

    ejercicio6

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,80 +1,64 @@
 package com.example;
 
-import lombok.EqualsAndHashCode;
 
 public class App 
 {
     public static void main( String[] args )
     {
-       Empleado[] arrayEmpleado = {
-    		   Empleado.builder()
-    		   .nombre("Maria")
-    		   .primerApellido("Lopez")
-    		   .segundoApellido("Martin")
-    		   .departamento(Departamento.FINANZAS)
-    		   .salario(3000.00)
-    		   .genero(Genero.MUJER)
-    		   .build(),  
-    		   Empleado.builder()
-    		   .nombre("Tamara")
-    		   .primerApellido("Diaz")
-    		   .segundoApellido("Martinez")
-    		   .departamento(Departamento.INFORMATICA)
-    		   .salario(5000.00)
-    		   .genero(Genero.MUJER)
-    		   .build(),
-    		   Empleado.builder()
-    		   .nombre("Ivan")
-    		   .primerApellido("Garcia")
-    		   .segundoApellido("Martin")
-    		   .departamento(Departamento.INFORMATICA)
-    		   .salario(4000.00)
-    		   .genero(Genero.HOMBRE)
-    		   .build()
-   
-       };
-    		   
-       System.out.println("El array Empleado");
-       for (Empleado empleado : arrayEmpleado)
-    	   System.out.println(empleado);
+      Object[] arrayempleadosYestudiantes = {
+    		Empleado.builder()
+   		   .nombre("Maria")
+   		   .primerApellido("Lopez")
+   		   .segundoApellido("Martin")
+   		   .departamento(Departamento.FINANZAS)
+   		   .salario(3000.00)
+   		   .genero(Genero.MUJER)
+   		   .build(),  
+   		   Empleado.builder()
+   		   .nombre("Tamara")
+   		   .primerApellido("Diaz")
+   		   .segundoApellido("Martinez")
+   		   .departamento(Departamento.INFORMATICA)
+   		   .salario(5000.00)
+   		   .genero(Genero.MUJER)
+   		   .build(),
+   		   Empleado.builder()
+   		   .nombre("Ivan")
+   		   .primerApellido("Garcia")
+   		   .segundoApellido("Martin")
+   		   .departamento(Departamento.INFORMATICA)
+   		   .salario(4000.00)
+   		   .genero(Genero.HOMBRE)
+   		   .build(),
+   		   Estudiantes.builder()
+   		   .nombre("Marly")
+   		   .facultad(Facultad.CIENCIAS)
+   		   .totalAsignaturasMatriculadas("5")
+   		   .build(),
+   		   Estudiantes.builder()
+		   .nombre("Carlos")
+		   .facultad(Facultad.INFORMATICA)
+		   .totalAsignaturasMatriculadas("8")
+		   .build(),
+		   Estudiantes.builder()
+   		   .nombre("Sergio")
+   		   .facultad(Facultad.ECONOMIA)
+   		   .totalAsignaturasMatriculadas("9")
+   		   .build()
+     };
        
-    double sumatoriaSalarioHombre = 0.0;
-    double sumatoriaSalarioMujer = 0.0;
-    double sumatoriaSalarioSinGenero = 0.0;
-    
-    int totalGeneroHombre = 0;
-    int totalGeneroMujer = 0;
-    int totalGeneroSinGenero = 0;
-    
-    double salarioPromedioHombres = 0.0;
-    double salarioPromedioMujeres = 0.0;
-    double salarioPromedioSinGenero = 0.0;
-    
-    for(Empleado e : arrayEmpleado)  {
-
-    if(e.getGenero().equals(Genero.HOMBRE)) {
-    	sumatoriaSalarioHombre += e.getSalario();
-    	totalGeneroHombre++;
-    } else if(e.getGenero().equals(Genero.MUJER)) {
-    		sumatoriaSalarioMujer += e.getSalario();
-    		totalGeneroMujer++;
-    	} else {
-    		sumatoriaSalarioSinGenero += e.getSalario();
-    		totalGeneroSinGenero++;
-    	}
-    }
-    
-    salarioPromedioHombres = sumatoriaSalarioHombre/ totalGeneroHombre;
-    salarioPromedioMujeres = sumatoriaSalarioMujer/ totalGeneroMujer;
-    salarioPromedioSinGenero = sumatoriaSalarioSinGenero / totalGeneroSinGenero;
-    
-    System.out.println("Salario promedio Hombres: " + salarioPromedioHombres);
-    System.out.println("Salario promedio Mujeres:" + salarioPromedioMujeres);
-    System.out.println("Salario promedio Sin Genero:" + salarioPromedioSinGenero);
-    
-    
-   	  
-    	
+      for(Object object: arrayempleadosYestudiantes) {
+    	  if(object instanceof Empleado) {
+    		  Empleado em = (Empleado) object;
+    		  System.out.println("Nombre del empleado:" + em.getNombre());
+    	  } else if(object instanceof Estudiantes) {
+    		  Estudiantes es = (Estudiantes) object;
+    		  System.out.println("Nombre del Estudiante:" + es.getNombre());
+    		  
+    	  }
+    	  Estudiantes[] arrayDeMujeres = new Estudiantes[6];
+    	  
+      }
     }
         
     
